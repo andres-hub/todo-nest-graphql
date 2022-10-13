650e259c997391dfa337bc046a433cf736609e8d
andres-hub <andrescalderoncl@gmail.com>
2022-10-13 15:37:51 -0500

    first commit

---
+++ src/hello-world/hello-world.resolver.ts
@@ -0,0 +1,23 @@
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
+
+@Resolver()
+export class HelloWorldResolver {
+
+    @Query(() => String, { description: 'Say hi', name: 'SayHi' })
+    helloWorld(): string {
+        return 'Hello world';
+    }
+
+    @Query(() => Float, { name: '+' })
+    getRandomNumber(): number {
+        return Math.random() * 100;
+    }
+
+    @Query(() => Int, { name: 'randomFromZeroTo', description: 'From zero to argument to (default 6)' })
+    getRandomFromZeroTo(
+        @Args('to', { nullable: true, type: () => Int }) to: number = 6
+    ): number {
+        return Math.floor(Math.random() * to) + 1;
+    }
+
+}
